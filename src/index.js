3de01076bc1d6a5c72dbceb6a8ddced33fc9e8a2
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-26 17:00:28 +0200

    Add most of the websit layout

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ let menu = new ElementCreator('a', 'nav__el', nav, "menu").getNodeByClass()
 let logo = new ElementCreator('a', 'nav__logo', nav).getNodeByClass()
 let img = new ElementCreator('img', 'imglogo', logo).getNodeByClass()
 img.src = "../src/img/sushi_lounge_logo.png";
+img.alt = "logo";
 let contact = new ElementCreator('a', 'nav__el', nav, "contact").getNodeByClass()
 let booknow = new ElementCreator('a', 'nav__el', nav, "book now").getNodeByClass()
 let info = new ElementCreator('div', 'info', container).getNodeByClass()
@@ -48,9 +49,11 @@ let mainHeading = new ElementCreator('div', 'main__heading', main).getNodeByClas
 let promo = new ElementCreator('span', 'main__promo', mainHeading, "promo").getNodeByClass()
 let h1 = new ElementCreator('h1', 'main__h1', mainHeading, "Amazing Table with River View & a lot of Menus").getNodeByClass()
 let btn = new ElementCreator('a', 'btn', mainHeading, "Check").getNodeByClass()
-
-
-// main__heading
+let side = new ElementCreator('div', 'side', info).getNodeByClass()
+let sideImgContainer = new ElementCreator('div', 'side__imgcontainer', side).getNodeByClass()
+let leftimage = new ElementCreator('img', 'side__img', sideImgContainer).getNodeByClass()
+leftimage.src = "../src/img/sushi.jpg";
+leftimage.alt = "sushi plate"
 
 
 
