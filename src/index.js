9c15c626bfddb3997ba83834f8b60894a8121a36
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-26 16:51:54 +0200

    Complete half of info section

--- src/index.js
+++ src/index.js
@@ -1,19 +1,58 @@
 import './style/style.scss';
 
+const content = document.getElementById('content');
+class ElementCreator {
+  constructor(elem, clss, parent, ...options) {
+    this.elem = elem;
+    this.clss = clss;
+    this.parent = parent;
+    this.options = [...options]
+    this.el = this.create(this.elem)
+    this.addClass(this.el)
+    this.addText()
+    this.parent.appendChild(this.el)
+  }
 
+  create(el) {
+    return document.createElement(el);
+  }
+  addClass(el) {
+    return el.classList.add(this.clss)
+  }
 
-const content = document.querySelector('.content');
+  getNodeByClass() {
+    return document.querySelector(`.${this.clss}`)
+  }
 
-// let nav = document.createElement('nav')
+  addText() {
+    if (this.options.length > 0) {
+      let text = this.options[0]
+      this.el.textContent = text
+    }
+  }
 
-function elemCreator(elem, clss) {
-  let el = document.createElement(elem);
-  let final = el.classList.add(clss)
-  return final
 }
+let container = new ElementCreator('div', 'container', content).getNodeByClass()
+let background = new ElementCreator('div', 'background', container).getNodeByClass()
+let nav = new ElementCreator('nav', 'nav', container).getNodeByClass()
+let about = new ElementCreator('a', 'nav__el', nav, "about").getNodeByClass()
+let menu = new ElementCreator('a', 'nav__el', nav, "menu").getNodeByClass()
+let logo = new ElementCreator('a', 'nav__logo', nav).getNodeByClass()
+let img = new ElementCreator('img', 'imglogo', logo).getNodeByClass()
+img.src = "../src/img/sushi_lounge_logo.png";
+let contact = new ElementCreator('a', 'nav__el', nav, "contact").getNodeByClass()
+let booknow = new ElementCreator('a', 'nav__el', nav, "book now").getNodeByClass()
+let info = new ElementCreator('div', 'info', container).getNodeByClass()
+let main = new ElementCreator('div', 'main', info).getNodeByClass()
+let mainHeading = new ElementCreator('div', 'main__heading', main).getNodeByClass()
+let promo = new ElementCreator('span', 'main__promo', mainHeading, "promo").getNodeByClass()
+let h1 = new ElementCreator('h1', 'main__h1', mainHeading, "Amazing Table with River View & a lot of Menus").getNodeByClass()
+let btn = new ElementCreator('a', 'btn', mainHeading, "Check").getNodeByClass()
 
 
-let nav = elemCreator('nav', 'nav')
-// let container = new ElemetCreator("div")
+// main__heading
 
-content.appendChild(nav)
+
+
+
+
