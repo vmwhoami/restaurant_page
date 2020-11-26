3fbcf5401aa0b2a621cd1db7a203856922e408e5
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-26 12:54:33 +0200

    start on implementing javascript

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import './style/style.scss';
 
 
 
+const content = document.querySelector('.content');
+
+// let nav = document.createElement('nav')
+
+function elemCreator(elem, clss) {
+  let el = document.createElement(elem);
+  let final = el.classList.add(clss)
+  return final
+}
+
+
+let nav = elemCreator('nav', 'nav')
+// let container = new ElemetCreator("div")
+
+content.appendChild(nav)
